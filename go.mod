2b09709563a3330a2a17972f4135c1fc1f1016dd
Qoft <63415260+Qoft@users.noreply.github.com>
2022-04-22 16:33:53 -0400

    ewrf

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/monkeyskid/Tiktok-shares-bot
+
+go 1.17
+
+require (
+	github.com/corpix/uarand v0.1.1
+	github.com/valyala/fasthttp v1.35.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	randomUserAgent "github.com/corpix/uarand"
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	client fasthttp.Client
+
+	errors int = 0
+	sent   int = 0
+	rpm    int = 0
+)
+
+func addShare(itemID string) {
+	req := fasthttp.AcquireRequest()
+	res := fasthttp.AcquireResponse()
+
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(res)
+
+	req.Header.SetMethod("POST")
+	req.SetRequestURI(generateURL())
+	req.SetBody([]byte(fmt.Sprintf("item_id=%v&share_delta=1", itemID)))
+
+	req.Header.Set("User-Agent", randomUserAgent.GetRandom())
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+	if err := client.Do(req, res); err != nil {
+		errors++
+		return
+	}
+
+	sent++
+}
+
+func rpmCounter() {
+	for {
+		before := sent
+		time.Sleep(100 * time.Millisecond)
+		after := sent
+
+		rpm = (after - before) * 600
+	}
+}
+
+func statusPrinter() {
+	for {
+		time.Sleep(time.Millisecond * 500)
+		fmt.Printf("[+] Sent: %v | Requests per minute: %v | Errors: %v\r", sent, rpm, errors)
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	fmt.Print("[+] github.com/qoft\n\n")
+
+	var threads int
+	var itemID string
+
+	fmt.Print("[>] Threads: ")
+	fmt.Scanln(&threads)
+
+	fmt.Print("[>] Video ID: ")
+	fmt.Scanln(&itemID)
+
+	fmt.Print("\n\n")
+
+	go rpmCounter()
+	go statusPrinter()
+
+	for i := 0; i < threads; i++ {
+		go func() {
+			for {
+				addShare(itemID)
+			}
+		}()
+	}
+
+	select {}
+}
